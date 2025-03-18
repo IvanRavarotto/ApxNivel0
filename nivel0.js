9c29918d61d439e011212ede94eb52718c97680b
IvanRavarotto <itravarotto@gmail.com>
2025-03-17 22:55:51 -0300

    Prueba

---
+++ nivel0.js
@@ -0,0 +1,16 @@
+/* 
+1 - Declarar una constante llamada unDulce y almacenar el nombre algún dulce que te guste
+2 - Declarar una variable ranking con un valor numérico (dependiendo de cuanto te guste) del 1 al 10
+3 - Mostrar ambos datos usando console.log()
+4 - Luego, modifica la variable ranking y asígnale otro valor. Otro número.
+5 - Imprimí ambos datos otra vez usando console.log()
+*/
+
+const unDulce = 'chocolate';
+let ranking = 10;
+
+console.log(unDulce, ranking);
+
+ranking = 8;
+
+console.log(unDulce, ranking);
