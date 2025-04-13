75909f4b1c342acd4077f8bf2bd9065c4cd02e8f
IvanRavarotto <itravarotto@gmail.com>
2025-04-12 23:30:01 -0300

    Se crea ejecercicio de mes actual

---
+++ mesactual.js
@@ -0,0 +1,9 @@
+// completá el array
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+// modificá esta constante para que indique el mes en el que estamos
+const mesActual = 11;
+
+// utiliza el array meses combinado con el número 
+// del mesActual para ver el mes en el que estamos
+// console.log(...)
+console.log(meses[mesActual - 1]);
