353d1805fad268a131b47cae9ba136e1e538d425
IvanRavarotto <itravarotto@gmail.com>
2025-04-14 23:06:20 -0300

    Se crea archivo yoobjeto.js con mis datos como un objeto

---
+++ nivel0/yoobjeto.js
@@ -0,0 +1,41 @@
+/*En este desafío la misión es crear un objeto que te represente y que incluya los siguientes datos.
+
+Nombre: string
+Apellido: string
+Edad: number
+Pais: string
+Profesión: string
+Sabe manejar: boolean
+Mascotas: array
+Hobbies: array
+Toca instrumentos: boolean
+Nivel de inglés: string o number
+Ciudad: string
+Redes sociales: objeto
+Películas favoritas: array
+Comidas favoritas: array
+Sabe cocinar: boolean
+Día de la semana favorito: string
+Objetivos por cumplir: array*/
+
+const yo = {
+    nombre: "Iván Tomás",
+    apellido: "Ravarotto",
+    edad: 23,
+    pais: "Argentina",
+    profesion: "Estudiante de programación",
+    sabeManejar: false,
+    mascotas: ["Gringa (labradora)", "Moro (labrador)","Colo (gato naranja)"],
+    hobbies: ["Jugar videojuegos", "Programar", "Escuchar música"],
+    tocaIntrumentos: false,
+    nivelIngres: "Basico",
+    ciudad: "Resistencia",
+    redesSociales: {
+        instagram: "ivantsrv"
+    },
+    peliculasFavoritas: ["El origen", "Interstellar", "El caballero de la noche"],
+    comidasFavoritas: ["Hamburguesa", "Pizza", "Asado"],
+    sabeCocinar: true,
+    diaSemanaFavorito: "Viernes",
+    objetivosPorCumplir: ["Viajar a Europa", "Aprender inglés", "Conseguir un trabajo de programador remoto"]
+}
